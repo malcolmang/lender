bff8f506999948b4ff1e1398b874d9b4a9b1e86f
Malcolm Ang <malcolmangsc@gmail.com>
2022-08-20 09:31:48 +0800

    Remove redundant code from merge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,56 +1,4 @@
 public class Main {
-<<<<<<< HEAD
-    static int[] amounts = new int[100];
-    static String[] borrowers = new String[100];
-    static boolean[] isPaid = new boolean[100];
-    static double[] interestRates = new double[100];
-    static String[] contactNumbers = new String[100];
-    static int loansCount = 0;
-
-    public static void addLoan(String borrower, int amount, double interestRate, String contactNumber) {
-        borrowers[loansCount] = borrower;
-        amounts[loansCount] = amount;
-        interestRates[loansCount] = interestRate;
-        contactNumbers[loansCount] = contactNumber;
-        loansCount++;
-    }
-
-    public static void setAsPaid(String borrower) {
-        for (int i = 0; i < loansCount; i++) {
-            if (borrowers[i].equals(borrower)) {
-                isPaid[i] = true;
-            }
-        }
-    }
-
-    public static void listLoans() {
-        for (int i = 0; i < loansCount; i++) {
-            if (!isPaid[i]) {
-                System.out.println(borrowers[i] + ": " + amounts[i]);
-            }
-        }
-    }
-
-    public static double calculateProfitEarned() {
-        double profit = 0;
-        for (int i = 0; i < loansCount; i++) {
-            // if the loan was repaid, add the interest to profit
-            if (isPaid[i]) {
-                profit = profit + (amounts[i] * interestRates[i]);
-            }
-        }
-        return profit;
-    }
-
-    public static void main(String[] args) {
-        addLoan("Alice Betsy", 10, 0.1, "37930339");
-        addLoan("Ben Chee", 20, 0.2, "03948243");
-        addLoan("Chris Davis", 30, 0.3, "20394021");
-        setAsPaid("Ben Chee");
-        listLoans();
-        System.out.println("profit: " + calculateProfitEarned());
-=======
-
     public static void main(String[] args) {
         LoanManager m1 = new LoanManager();
         m1.addLoan("Alice Betsy", 10, 0.1);
@@ -66,6 +14,5 @@ public class Main {
         m2.setAsPaid("Alice Betsy");
         m2.listLoans();
         System.out.println("profit from m2: " + m2.calculateProfitEarned());
->>>>>>> cs2113/master
     }
 }
